ee24978b0824c9b8c3bcbb9a72c5a65ae2e18384
Jason Theiler <turret.jason@outlook.com>
2021-11-17 17:27:55 +0100

    feat: darken scrollbar thumb

--- App.vue
+++ App.vue
@@ -12,6 +12,6 @@
 }
 
 body {
-  @apply bg-dark-900 text-gray-300 scrollbar-thin scrollbar-track-transparent scrollbar-thumb-rounded-full scrollbar-thumb-dark-300 hover:(scrollbar-thumb-dark-100);
+  @apply bg-dark-900 text-gray-300 scrollbar-thin scrollbar-track-transparent scrollbar-thumb-rounded-full scrollbar-thumb-dark-400 hover:(scrollbar-thumb-dark-200);
 }
 </style>
