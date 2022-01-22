e6fa8fc1ae0a9d618331235222b73364c0b803e2
Jason Theiler <awd>
2022-01-22 22:51:10 +0100

    chore: update deps
    - refactor: simplify module types

--- types/modules.d.ts
+++ types/modules.d.ts
@@ -1,5 +1,3 @@
 declare module "*.md" {
-  import { DefineComponent } from "vue";
-  const content: Record<string, any> & { body: DefineComponent<{}, {}, any> };
-  export default content;
+  export default Content;
 }
