541246c420eab10e503f573f0c065494eba983f5
Jason Theiler <turret.jason@outlook.com>
2022-03-25 02:48:35 +0100

    feat: try out Trois.js

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,32 @@
-<template>
-  <main class="mt-32">
-    <TheWipNotice />
+<script lang="ts" setup>
+import {
+  Box,
+  Camera,
+  LambertMaterial,
+  PointLight,
+  Renderer,
+  Scene,
+} from "troisjs";
+</script>
 
-    <TheProjects />
+<template>
+  <main class="h-screen">
+    <ClientOnly>
+      <Renderer resize alpha antialias orbitCtrl>
+        <Camera :position="{ z: 10 }" />
+
+        <Scene>
+          <PointLight
+            :position="{ x: -25, y: -75, z: -50 }"
+            :intensity="0.25"
+          />
+          <PointLight :position="{ x: 25, y: 75, z: 50 }" />
+
+          <Box :size="4.5">
+            <LambertMaterial />
+          </Box>
+        </Scene>
+      </Renderer>
+    </ClientOnly>
   </main>
 </template>
