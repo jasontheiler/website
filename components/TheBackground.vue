0ac0ef50a4dd78c7d1f6419fe171c19af60206ff
Jason Theiler <turret.jason@outlook.com>
2022-03-29 20:43:57 +0200

    chore: update deps
    - chore: remove and clean up TroisJS

--- components/TheBackground.vue
+++ components/TheBackground.vue
@@ -1,15 +1,3 @@
-<script lang="ts" setup>
-import { Camera, Renderer, Scene } from "troisjs";
-</script>
-
 <template>
-  <div class="fixed inset-0 -z-1">
-    <ClientOnly>
-      <Renderer resize alpha antialias>
-        <Camera :position="{ z: 10 }" />
-
-        <Scene></Scene>
-      </Renderer>
-    </ClientOnly>
-  </div>
+  <div class="fixed inset-0 -z-1"></div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,6 @@ import { defineNuxtConfig } from "nuxt3";
 
 // See: https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-  build: {
-    transpile: ["troisjs"],
-  },
-
   buildModules: [
     // See: https://github.com/antfu/unocss
     "@unocss/nuxt",
